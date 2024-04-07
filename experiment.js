c6fce2424c186d1edc70e004d6e0a75aa5774059
Raghav B. Venkataramaiyer <bvraghav@gmail.com>
2024-04-07 07:41:10 +0530

    Update for group details.

--- experiment.js
+++ experiment.js
@@ -1,7 +1,7 @@
 class Experiment {
   // Group Details
-  static rollNo = '10983437,10983743'
-  static name = 'The Tutors(Akhtar Banga, Phul Tekchand)'
+  static rollNos = '10983437,10983743'
+  static names = 'The Tutors(Akhtar Banga, Phul Tekchand)'
 
   canvasSel = '#myCanvas'
 
--- main.js
+++ main.js
@@ -34,27 +34,75 @@ function main() {
 }
 
 // FIXME: Relax the validators for multiple names
-function updateCandidateDetails({rollNo,name}) {
-  let isValidRollNo, isValidName
+function updateCandidateDetails({rollNos,names}) {
+  let isValidRollNo, isValidName, act, byMessage, n
   isValidRollNo = (isValidName = false)
 
   // Validate RollNo
-  rollNo = Number(rollNo)
-  isValidRollNo = !isNaN(rollNo) && 9999999 < rollNo
-  if (!isValidRollNo) {
-    console.warn({invalidRollNo: rollNo})
-  }
+  
+  rollNos = rollNos.split(',')
+    .map(
+      s => Number(s.trim())
+    )
+    .filter(
+      n => {
+	isValidRollNo = !isNaN(n) && 9999999 < n
+	if (!isValidRollNo) {
+	  console.warn({invalidRollNo: n,
+			message: "Roll No should be Integer."})
+	}
+	return isValidRollNo
+      }
+    )
 
   // Validate Name
   const titleCasePat = /^[A-Z][a-z]+( [A-Z][a-z]+)*$/
-  name = String(name).trim()
-  isValidName = titleCasePat.test(name)
+  names = String(names).trim()
+
+  act = names.split('(').shift().trim()
+  isValidName = titleCasePat.test(act)
   if (!isValidName) {
-    console.warn({invalidName: name})
+    console.warn({invalidAct: act,
+		  message: "Act should be in Title Case."})
+    act = ''
   }
+
+  names = names.split('(').pop().split(')').shift()
+    .split(',').map(s=>s.trim())
+    .filter(name => {
+      isValidName = titleCasePat.test(name)
+      if (!isValidName) {
+	console.warn({invalidName: name,
+		      message: "Name should be a Title Case."})
+      }
+      return isValidName
+    })
   
-  if (isValidRollNo && isValidName) {
-    document.querySelector('#by')
-      .textContent = `By: ${name} (${rollNo})`
+  byMessage = ''
+
+  n = Math.min(names.length, rollNos.length)
+  for (const i of Array(n).keys()) {
+    if (0 < i)
+      byMessage = `${byMessage}, `
+
+    byMessage = `${byMessage}${rollNos[i]}:${names[i]}`
   }
+
+  if (0 < byMessage.length) {
+    byMessage = `(${byMessage})`
+  }
+
+  if (0 < act.length) {
+    byMessage = `${act} ${byMessage}`
+  }
+
+  if (0 < byMessage.length) {
+    byMessage = `By: ${byMessage}`
+  } else {
+    byMessage
+      = 'Error parsing candidate details. Check console.'
+  }
+
+  document.querySelector('#by')
+    .textContent = byMessage
 }
