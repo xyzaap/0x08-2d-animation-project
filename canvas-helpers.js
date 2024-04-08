15d1b88ec51a59f298c02a8f86c3faad74fb76df
Raghav B. Venkataramaiyer <bvraghav@gmail.com>
2024-04-08 06:43:27 +0530

    Update canvas to be effectively wide before reset.
    Use setTimeout to wait for the update to take effect
    prior to reset.

--- canvas-helpers.js
+++ canvas-helpers.js
@@ -3,8 +3,13 @@ const getDom = document.querySelector.bind(document)
 
 function canvasSetup(sel) {
   const {canvas, bb:{width: W, height: H}} = getCanvas(sel)
-  canvas.width = parseFloat(W)
-  canvas.height = parseFloat(H)
+  canvas.width = 10
+  canvas.height = 1
+
+  setTimeout(() => {
+    canvas.width = parseFloat(W)
+    canvas.height = parseFloat(H)
+  }, 0)
 }
 
 function getCanvas(sel, context='2d') {
